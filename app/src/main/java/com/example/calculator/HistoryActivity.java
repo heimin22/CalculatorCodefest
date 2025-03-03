2c60a8b9685898c0895339ab231d98d895528ebb
jeru <john.ungab7@gmail.com>
2025-03-03 17:42:40 +0800

    add: history button method

--- app/src/main/java/com/example/calculator/HistoryActivity.java
+++ app/src/main/java/com/example/calculator/HistoryActivity.java
@@ -1,9 +1,6 @@
 package com.example.calculator;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,36 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button historyBtn;
+public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_history);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        InitializeComponents();
-    }
-
-    /**
-     * Initializes components
-     * */
-    private void InitializeComponents()
-    {
-//        historyBtn: ref sa history button
-//        historyBtn = findViewById(R.id.historyBtn);
-        historyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
